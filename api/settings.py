8c19a8b8bfcd4648444ba11668c2b1cd263f97bf
Remis Haroon <remis.haroon@pwc.com>
2016-01-31 11:56:41 +0000

    changed vars

--- api/settings.py
+++ api/settings.py
@@ -59,7 +59,7 @@ MEDIA_URL = os.environ.get('MEDIA_URL', '')
 # Don't put anything in this directory yourself; store your static files
 # in apps' 'static/' subdirectories and in STATICFILES_DIRS.
 # Example: '/home/media/media.lawrence.com/static/'
-STATIC_ROOT = os.path.join(BASE_PATH, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: 'http://media.lawrence.com/static/'
@@ -80,7 +80,7 @@ STATICFILES_FINDERS = (
 
 #  Make this unique, and don't share it with anybody.
 if not TESTING:
-    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    SECRET_KEY = os.getenv('DJANGO_SECRET_KEY','^b46d^zsxhrthkq2p7ty14^zov%4@eq@j5e%f(ki_#2rnbp%-l')
 else:
     SECRET_KEY = 'secret'
 
